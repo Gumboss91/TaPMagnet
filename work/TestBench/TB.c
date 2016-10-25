85d02cec49fdaaf3a99ab132545291e3e82ad95b
gumboss <gumboss@web.de>
2016-10-26 00:16:35 +0200

    up

--- work/TestBench/TB.c
+++ work/TestBench/TB.c
@@ -2,16 +2,17 @@
 #include "TB.h"
 
 int main(int argc,char* argv[]){
-	int debug = 0;
-	printBuff = atoi(argv[2]);
-	debug = 1;
-	Sleep = atoi(argv[1]);
-	init = 0;
+	int debug = atoi(argv[1];
 	
+	Sleep = atoi(argv[2]);
+	printBuff = atoi(argv[3]);
+		
+	init = 0;
+	printf("---Start TB---\n");
 	
 	tbTLC59711(debug);
-	
-	
-	tbTaPMagnets(debug);						
+	tbTaPMagnets(debug);
+
+	printf("---End TB---\n")
 	return 0;
 }
