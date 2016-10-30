54103c1d63ad13799e7e9099c89e0928fc0daa50
Gumboss91 <gumboss@web.de>
2016-10-30 13:02:20 +0000

    update arguments

--- work/TaPMagnetServer.c
+++ work/TaPMagnetServer.c
@@ -1,27 +1,39 @@
+#include <unistd.h>
 #include "TaPMagnetServer.h"
 
-int main(int argc, char *argv[]){
+
+extern char *optarg;
+main(int argc, char *argv[]){
 	int opt;
 	int port;
 	int debug;
-	
-	while ((opt = getopt(argc, argv, "pd")) != -1) {
+	port = DEFAULTPORT;
+	debug = 1;
+
+	while ((opt = getopt(argc, argv, "p:dh")) != -1) {
 		switch (opt) {
 		case 'p':
-            port = atoi(optarg);
-			if(port == NULL)
-				port = DEFAULTPORT;
-            break;
-        case 'd':
-            debug = 1;
-            break;
-        default: /* '?' */
-            printf("Usage: %s [-t nsecs] [-n] name\n", argv[0]);
-            return -2;
-        }
+			if(optarg != NULL)
+				port = atoi(optarg);
+			break;
+		case 'd':
+			debug = 1;
+			break;
+		case 'h':
+			printf("Help for TaPMagnetServer\n");
+			printf("Usage: %s [-p port number] [-d] debug mode [-h] show Help\n", argv[0]);
+			return 0;
+			break;
+		default: /* '?' */
+			printf("Usage: %s [-p portnumber] [-d] debug mode [-h] show Help\n", argv[0]);
+			return -2;
     }
+  }
 	printf("port: %d\n",port);
 	printf("debug: %d\n",debug);
 	
+	
+
+
 	return 0;
 }
